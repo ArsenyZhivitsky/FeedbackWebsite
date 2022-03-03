9d4c637e6159a3c386b6d2d6f9651ab25945d92b
ar.zhivitskij2011@yandex.by <ar.zhivitskij2011@yandex.by>
2022-03-04 02:45:46 +0300

    Role initializer is deleted

--- FeedbackWebsite/FeedbackWebsite/Program.cs
+++ FeedbackWebsite/FeedbackWebsite/Program.cs
@@ -15,22 +15,6 @@ namespace FeedbackWebsite
         {
             var host = BuildWebHost(args);
 
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var userManager = services.GetRequiredService<UserManager<User>>();
-                    var rolesManager = services.GetRequiredService<RoleManager<IdentityRole>>();
-                    await RoleInitializer.InitializeAsync(userManager, rolesManager);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while seeding the database.");
-                }
-            }
-
             host.Run();
         }
 
